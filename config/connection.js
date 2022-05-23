1ef7022c1f53cbc1e5a1c8626dff32f258583edd
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-05-23 18:30:02 -0500

    Create schema, populate .env with mysql login and export sequelize from connection.js

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize('tech_talk', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;
