5a5722645e57b206134df9035a935349af8fbd33
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-05-23 23:11:10 -0500

    Fix issue with process.env in sequelize

--- config/connection.js
+++ config/connection.js
@@ -10,13 +10,26 @@ require('dotenv').config();
 
 
 const sequelize = process.env.JAWSDB_URL
-  new Sequelize(process.env.JAWSDB_URL)
-  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     dialectOptions: {
       decimalNumbers: true,
     },
   });
+// The code block below works similarly to the block above. Both execute.
+
+// let sequelize;
+// 
+// if (process.env.JAWSDB_URL) {
+  // sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+  // sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    // host: 'localhost',
+    // dialect: 'mysql',
+    // port: 3306
+  // });
+// }
 
  module.exports = sequelize;
