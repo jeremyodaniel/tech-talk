2cc6584202cc192674c760828221ac4dfff507c5
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-05-26 21:45:32 -0500

    Update routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 
+
 const userRoutes = require('./user-routes.js');
 
+// ADD THE MISSING ROUTES AND  MODELS FOR THESE!!!
+
+// const postRoutes = require('./post-routes.js');
+// const commentRoutes = require('./comment-routes.js');
+
+
 router.use('/users', userRoutes);
+// router.use('/posts', postRoutes);
+// router.use('/comments', commentRoutes);
 
 module.exports = router;
