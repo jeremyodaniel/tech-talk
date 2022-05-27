5a6e6508c73b0f10bab8d16afc5e6f78d15fb46f
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-05-26 20:06:10 -0500

    Add stylesheets & middleware in server.js to connect to localhost

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
